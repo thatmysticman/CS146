c6e3fe4e40d5e2dea91c2504377fd43f7c457324
thatmysticman <zshake21@gmail.com>
2024-02-01 15:11:57 -0800

    Update lab1.py

--- Labs/Lab 1/lab1.py
+++ Labs/Lab 1/lab1.py
@@ -1,1 +1,12 @@
+def two_sum(nums, target):
+    result = [0, 0]
 
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                result[0] = i
+                result[1] = j
+                return result
+
+    # If no solution is found, return an empty list
+    return []
