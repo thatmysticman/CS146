07d9608bbe44de3f4d5e30c90c04d3a02d0776d8
thatmysticman <zshake21@gmail.com>
2024-02-04 17:06:06 -0800

    Update lab1.java

--- Labs/Lab 1/lab1.java
+++ Labs/Lab 1/lab1.java
@@ -1,5 +1,4 @@
-public class TwoSum {
-
+public class Solution {
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
 
@@ -15,13 +14,6 @@ public class TwoSum {
 
         // If no solution is found, return an empty array
         return new int[0];
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int[] result = twoSum(nums, target);
-        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
     }
 }
 
