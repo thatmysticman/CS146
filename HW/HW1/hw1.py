a1d056920d44f1abce48cf4e14f6f4e8d1f5bb05
thatmysticman <zshake21@gmail.com>
2024-02-04 16:12:47 -0800

    Update hw1.py

--- HW/HW1/hw1.py
+++ HW/HW1/hw1.py
@@ -1,1 +1,12 @@
+def is_palindrome(s):
+    # Convert to lowercase and remove non-alphanumeric characters
+    cleaned_string = ''.join(c.lower() for c in s if c.isalnum())
 
+    # Check if the cleaned string is a palindrome
+    left, right = 0, len(cleaned_string) - 1
+    while left < right:
+        if cleaned_string[left] != cleaned_string[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
