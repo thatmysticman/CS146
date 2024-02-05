82ef173de55d48c2008954c35e41d03bc8eca167
thatmysticman <zshake21@gmail.com>
2024-02-04 16:38:51 -0800

    Update hw2.java

--- HW/HW2/hw2.java
+++ HW/HW2/hw2.java
@@ -1,1 +1,22 @@
+public class Solution {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    // This is a placeholder for the isBadVersion method.
+    // You should replace it with the actual implementation provided by the API.
+    private boolean isBadVersion(int version) {
+        // Your implementation here.
+        return false;
+    }
+}
 
