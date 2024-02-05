cf5987882649d1c630bc5ac898565fa44e99b210
thatmysticman <zshake21@gmail.com>
2024-02-04 16:02:35 -0800

    Update hw1.java

--- HW/HW1/hw1.java
+++ HW/HW1/hw1.java
@@ -1,1 +1,18 @@
+public static boolean isPalindrome(String s) {
+    // Convert to lowercase and remove non-alphanumeric characters
+    String cleanedString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
 
+    // Check if the cleaned string is a palindrome
+    int left = 0;
+    int right = cleanedString.length() - 1;
+
+    while (left < right) {
+        if (cleanedString.charAt(left) != cleanedString.charAt(right)) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
