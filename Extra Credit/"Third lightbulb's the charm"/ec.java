46ebbc776024df71fb9bbb53cbbec7a5e24418f7
Zaki Sheikh <zshake21@gmail.com>
2024-02-08 13:46:04 -0800

    Update ec.java

--- Extra Credit/"Third lightbulb's the charm"/ec.java
+++ Extra Credit/"Third lightbulb's the charm"/ec.java
@@ -1,1 +1,17 @@
+public class Solution {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
 
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib[n];
+    }
+}
