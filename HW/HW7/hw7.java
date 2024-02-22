c32ac50d7d5207f307ebd77e478975607bcc548d
Zaki Sheikh <zshake21@gmail.com>
2024-02-22 00:45:59 -0800

    Update hw7.java

--- HW/HW7/hw7.java
+++ HW/HW7/hw7.java
@@ -1,1 +1,18 @@
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
 
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> endTimeMinHeap = new PriorityQueue<>();
+
+        for (int[] interval : intervals) {
+            if (!endTimeMinHeap.isEmpty() && endTimeMinHeap.peek() <= interval[0]) {
+                endTimeMinHeap.poll();
+            }
+            endTimeMinHeap.offer(interval[1]);
+        }
+
+        return endTimeMinHeap.size();
+    }
