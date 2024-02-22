7d070ee7ac512fb56417ce777fe6a8148bbb58a5
Zaki Sheikh <zshake21@gmail.com>
2024-02-22 00:45:40 -0800

    Update hw7.py

--- HW/HW7/hw7.py
+++ HW/HW7/hw7.py
@@ -1,1 +1,14 @@
+def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+    if not intervals:
+        return 0
 
+    intervals.sort(key=lambda x: (x[0], x[1])) 
+
+    endTimeMinHeap = []
+
+    for interval in intervals:
+        if endTimeMinHeap and endTimeMinHeap[0] <= interval[0]:
+            heapq.heappop(endTimeMinHeap)
+        heapq.heappush(endTimeMinHeap, interval[1])
+
+    return len(endTimeMinHeap)
