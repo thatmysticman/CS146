b52cc29890fae985439a80c2f1bd3216da1c7539
Zaki Sheikh <zshake21@gmail.com>
2024-02-22 23:07:05 -0800

    Update hw8.java

--- HW/HW8/hw8.java
+++ HW/HW8/hw8.java
@@ -1,1 +1,28 @@
+public class Solution {
+    public int longestPalindrome(String s) {
+        int[] charCount = new int[128]; // Assuming ASCII characters
+        
+        for (char c : s.toCharArray()) {
+            charCount[c]++;
+        }
+        
+        int palindromeLength = 0;
+        boolean oddCountFound = false;
+        
+        for (int count : charCount) {
+            palindromeLength += (count / 2) * 2;
+            
+            if (count % 2 == 1) {
+                oddCountFound = true;
+            }
+        }
+        
+        return palindromeLength + (oddCountFound ? 1 : 0);
+    }
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("abccccdd")); // Output: 7
+        System.out.println(solution.longestPalindrome("speediskey")); // Output: 5
+    }
+}
