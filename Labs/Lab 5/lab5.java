bf3288ddb9cf36cad1d59e4847ff565d72d85523
Zaki Sheikh <zshake21@gmail.com>
2024-03-07 15:22:01 -0800

    Update lab5.java

--- Labs/Lab 5/lab5.java
+++ Labs/Lab 5/lab5.java
@@ -1,1 +1,15 @@
+public boolean isValidBST(TreeNode root) {
+    return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+}
 
+private boolean isValidBSTHelper(TreeNode node, long min, long max) {
+    if (node == null) {
+        return true;
+    }
+    
+    if (node.val <= min || node.val >= max) {
+        return false;
+    }
+    
+    return isValidBSTHelper(node.left, min, node.val) && isValidBSTHelper(node.right, node.val, max);
+}
