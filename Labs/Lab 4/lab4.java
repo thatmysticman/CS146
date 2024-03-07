c7f4453e84ef1fec80246671c75a294c923df6e2
Zaki Sheikh <zshake21@gmail.com>
2024-03-07 14:56:49 -0800

    Update lab4.java

--- Labs/Lab 4/lab4.java
+++ Labs/Lab 4/lab4.java
@@ -1,1 +1,17 @@
+public void reverseTree(TreeNode root) {
+    if (root == null) {
+        return;
+    }
 
+    TreeNode temp = root.right;
+    root.right = root.left;
+    root.left = temp;
+
+    reverseTree(root.left);
+    reverseTree(root.right);
+}
+
+public TreeNode invertTree(TreeNode root) {
+    reverseTree(root);
+    return root;
+}
