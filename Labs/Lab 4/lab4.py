3d0bc1152e694391d75f5af179f67de762d203d6
Zaki Sheikh <zshake21@gmail.com>
2024-03-07 14:51:43 -0800

    Update lab4.py

--- Labs/Lab 4/lab4.py
+++ Labs/Lab 4/lab4.py
@@ -1,1 +1,14 @@
+def reverseTree(self, root):
+        if root is None:
+            return
 
+        temp = root.right
+        root.right = root.left
+        root.left = temp
+
+        self.reverseTree(root.left)
+        self.reverseTree(root.right)
+
+    def invertTree(self, root):
+        self.reverseTree(root)
+        return root
