925afe5b171a3fc959b961201e8180c82a6f33d4
Zaki Sheikh <zshake21@gmail.com>
2024-03-07 15:21:20 -0800

    Update lab5.py

--- Labs/Lab 5/lab5.py
+++ Labs/Lab 5/lab5.py
@@ -1,1 +1,12 @@
+def isValidBST(self, root):
+    return self.isValidBSTHelper(root, float('-inf'), float('inf'))
 
+def isValidBSTHelper(self, node, min_val, max_val):
+    if not node:
+        return True
+    
+    if node.val <= min_val or node.val >= max_val:
+        return False
+    
+    return (self.isValidBSTHelper(node.left, min_val, node.val) and
+            self.isValidBSTHelper(node.right, node.val, max_val))
