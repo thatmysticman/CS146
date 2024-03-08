65865ef6c957a8b3271784c90d216e47d43c6cf1
Zaki Sheikh <zshake21@gmail.com>
2024-03-07 20:19:06 -0800

    Update hw9.py

--- HW/HW9/hw9.py
+++ HW/HW9/hw9.py
@@ -1,1 +1,20 @@
-
+def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    if not root:
+        return None
+    
+    root_val = root.val
+    p_val = p.val
+    q_val = q.val
+    
+    if p_val < -1000000000 or p_val > 1000000000 or q_val < -1000000000 or q_val > 1000000000:
+        raise ValueError("Node value out of bounds")
+    
+    if p_val == q_val:
+        raise ValueError("p and q should be distinct nodes")
+        
+    if p_val < root_val and q_val < root_val:
+        return self.lowestCommonAncestor(root.left, p, q)
+    elif p_val > root_val and q_val > root_val:
+        return self.lowestCommonAncestor(root.right, p, q)
+    else:
+        return root
