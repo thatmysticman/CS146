fb0b079a134c4b83963af650de3d15eefc036ef3
Zaki Sheikh <zshake21@gmail.com>
2024-03-07 20:25:59 -0800

    Update hw9.java

--- HW/HW9/hw9.java
+++ HW/HW9/hw9.java
@@ -1,1 +1,25 @@
-
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null) {
+        return null;
+    }
+    
+    int rootVal = root.val;
+    int pVal = p.val;
+    int qVal = q.val;
+    
+    if (pVal < -1000000000 || pVal > 1000000000 || qVal < -1000000000 || qVal > 1000000000) {
+        throw new IllegalArgumentException("Node value out of bounds");
+    }
+    
+    if (pVal == qVal) {
+        throw new IllegalArgumentException("p and q should be distinct nodes");
+    }
+    
+    if (pVal < rootVal && qVal < rootVal) {
+        return lowestCommonAncestor(root.left, p, q);
+    } else if (pVal > rootVal && qVal > rootVal) {
+        return lowestCommonAncestor(root.right, p, q);
+    } else {
+        return root;
+    }
+}
