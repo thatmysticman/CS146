8da80660e16ff5f44d50aebbcb3383559fb0fa42
Zaki Sheikh <zshake21@gmail.com>
2024-03-14 17:51:14 -0700

    Update hw10.py

--- HW/HW10/hw10.py
+++ HW/HW10/hw10.py
@@ -1,1 +1,24 @@
-
+def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    if not root:
+        return None
+    
+    result = []
+    queue = deque([root])
+    
+    while queue:
+        level_size = len(queue)
+        level_nodes = []
+        
+        for _ in range(level_size):
+            node = queue.popleft()
+            level_nodes.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+                
+            if node.right:
+                queue.append(node.right)
+        
+        result.append(level_nodes)
+    
+    return result
