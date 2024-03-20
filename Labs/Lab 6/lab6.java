8aa47fe9db9ae2455e3b1a5b9e0f10dd0c112b86
Zaki Sheikh <zshake21@gmail.com>
2024-03-20 12:36:47 -0700

    Update lab6.java

--- Labs/Lab 6/lab6.java
+++ Labs/Lab 6/lab6.java
@@ -1,1 +1,38 @@
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // Construct the graph
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prereq = prerequisite[1];
+            graph.putIfAbsent(course, new ArrayList<>());
+            graph.get(course).add(prereq);
+        }
 
+        int[] visited = new int[numCourses];
+
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, visited, graph))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int course, int[] visited, Map<Integer, List<Integer>> graph) {
+        if (visited[course] == -1)
+            return false;
+        if (visited[course] == 1)
+            return true;
+
+        visited[course] = -1;
+
+        if (graph.containsKey(course)) {
+            for (int neighbor : graph.get(course)) {
+                if (!dfs(neighbor, visited, graph))
+                    return false;
+            }
+        }
+
+        visited[course] = 1;
+        return true;
+    }
