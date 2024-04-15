915344041cec79f5f12fa18f4d89ecf6f0617224
Zaki Sheikh <zshake21@gmail.com>
2024-04-15 12:42:08 -0700

    Update lab7.java

--- Labs/Lab 7/lab7.java
+++ Labs/Lab 7/lab7.java
@@ -1,12 +1,10 @@
 public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-    // Initialize the distance matrix with max value and 0 for diagonal elements
     int[][] distance = new int[n][n];
     for (int i = 0; i < n; i++) {
         Arrays.fill(distance[i], Integer.MAX_VALUE);
         distance[i][i] = 0;
     }
 
-    // Populate the distance matrix with edge weights
     for (int[] edge : edges) {
         int from = edge[0];
         int to = edge[1];
@@ -15,7 +13,6 @@ public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         distance[to][from] = weight;
     }
 
-    // Floyd-Warshall algorithm to find the shortest paths between all pairs of cities
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -29,7 +26,6 @@ public int findTheCity(int n, int[][] edges, int distanceThreshold) {
     int minCity = -1;
     int minReachable = n;
     
-    // Iterate through all the cities and count reachable cities
     for (int i = 0; i < n; i++) {
         int reachable = 0;
         for (int j = 0; j < n; j++) {
